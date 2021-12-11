c8e1295166d6cb092a6fa38c89458c4bd1e73564
Git IY! <91715524+gitiy1@users.noreply.github.com>
2021-12-11 09:55:58 +0800

    Update winner.sh

--- winner.sh
+++ winner.sh
@@ -1,2 +1,3 @@
 wget https://github.com/cloudreve/Cloudreve/releases/download/3.4.2/cloudreve_3.4.2_linux_amd64.tar.gz
 tar -zxvf cloudreve_3.4.2_linux_amd64.tar.gz && chmod +x ./cloudreve && ./cloudreve
+echo "hello"
