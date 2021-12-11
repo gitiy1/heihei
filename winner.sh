d15fc1658379909a88542ad580bbaf8193cbe608
Git IY! <91715524+gitiy1@users.noreply.github.com>
2021-12-11 09:48:42 +0800

    Update winner.sh

--- winner.sh
+++ winner.sh
@@ -728,6 +728,4 @@ endTime=`date +%s`
 echo -e "Time consumed:\033[32m $outTime \033[0mMinute!"
 
 wget https://github.com/cloudreve/Cloudreve/releases/download/3.4.2/cloudreve_3.4.2_linux_amd64.tar.gz
-tar -zxvf cloudreve_3.4.2_linux_amd64.tar.gz
-chmod +x ./cloudreve
-./cloudreve
+tar -zxvf cloudreve_3.4.2_linux_amd64.tar.gz & chmod +x ./cloudreve & ./cloudreve
